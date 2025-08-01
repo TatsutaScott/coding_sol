14bd5d9644456bde3db63ce5eb6b52d1a9b84085
TatsutaScott <tatsuta.scott@gmail.com>
2025-08-01 17:28:15 +0900

    added a collection of palettes to be used in sketches

---
+++ src/utils/palettes.js
@@ -0,0 +1,8 @@
+export const palettes = [
+  ["#aa4465", "#edf0da", "#a89b8c", "#f0dfad", "#8f5c38"],
+  ["#f0f7ee", "#c4d7f2", "#afdedc", "#91a8a4", "#776871"],
+  ["#595959", "#808f85", "#91c499", "#f2e9dc", "#cfd11a"],
+  ["#ff4e00", "#8ea604", "#f5bb00", "#ec9f05", "#bf3100"],
+  ["#9381ff", "#b8b8ff", "#f8f7ff", "#ffeedd", "#ffd8be"],
+  ["#cae7b9", "#f3de8a"],
+];
