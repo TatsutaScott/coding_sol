a10ddbb770cbffb4494e3e85956b4e97abb9cfcd
TatsutaScott <tatsuta.scott@gmail.com>
2025-08-02 22:19:44 +0900

    added test page for simple development

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import home_view from "@/view/home_view.vue";
 import project_view from "@/view/project_view.vue";
+import test_view from "@/view/test_view.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", component: home_view },
     { path: "/wall-drawing/:number", component: project_view },
+    { path: "/test", component: test_view },
   ],
 });
 
---
+++ src/view/test_view.vue
@@ -0,0 +1,60 @@
+<template>
+  <main>
+    <div id="container" ref="container"></div>
+  </main>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+import Canvas from "../../public/utils/Canvas";
+const container = ref();
+let cnv, draw;
+
+onMounted(async () => {
+  //find the number of the current wall drawing from the url
+  const drawing_number = 46; // <--wall drawing to test
+
+  //import drawing function
+  const wall_drawing_module = await import(
+    `/wall_drawings/wall_drawing_${drawing_number}.js`
+  );
+  draw = wall_drawing_module.draw;
+
+  //setup canvas and draw initial drawing
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  cnv = new Canvas(container.value, width, height);
+  draw(cnv);
+
+  //add eventlistener for redrawing
+  window.addEventListener("keydown", handleKeydown);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", handleKeydown);
+});
+
+function handleKeydown(e) {
+  if (e.code === "Space") {
+    draw(cnv);
+  }
+}
+</script>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+}
+
+#container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
